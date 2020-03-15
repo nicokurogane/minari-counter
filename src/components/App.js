1e8e9f4085fd0733c2d41448ad53f43e184657fd
Nicolas Martinez <fyedream@gmail.com>
2020-03-15 08:35:58 -0600

    🏗 Moved App.js to components folder

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserInfo from "components/userInfo/UserInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfo />
+    </div>
+  );
+}
+
+export default App;
